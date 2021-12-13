184aec207d1505ad1b1bfd6ef05b5a38f2ab043c
thanh <hodienthanh123@gmail.com>
2021-12-13 16:29:48 +0700

    thanh_4

--- app/src/main/java/com/example/tiki/module/entity/Response.java
+++ app/src/main/java/com/example/tiki/module/entity/Response.java
@@ -1,22 +1,2 @@
-package com.example.tiki.module.entity;
-
-import com.google.gson.annotations.SerializedName;
-
-public class Response123 {
-
-	@SerializedName("data")
-	private AllData data;
-
-	@SerializedName("status")
-	private int status;
-
-	public AllData getData(){
-		return data;
-	}
-
-	public int getStatus(){
-		return status;
-	}
-
-
+package com.example.tiki.module.entity;public class Response {
 }
